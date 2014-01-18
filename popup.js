86d8f15701bcca16d36edd7d6a9c7cda8d0a8c90
Colm Britton <colmjude@gmail.com>
2014-01-18 10:04:29 +0000

    reformat to 4 spaces a tab

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 // Run our kitten generation script as soon as the document's DOM is ready.
 // document.addEventListener('DOMContentLoaded', function () {
 //   // kittenGenerator.requestKittens();
@@ -10,43 +9,42 @@ function TiddlyChrome() {
 }
 
 TiddlyChrome.saveTiddler = function() {
-   // var space = new Space(baseURL, spaceName, this);
-   var tiddler = TiddlyChrome.readTiddler();
-   console.log(tiddler);
-   TiddlyChrome.putTiddler(tiddler);
+    // var space = new Space(baseURL, spaceName, this);
+    var tiddler = TiddlyChrome.readTiddler();
+    console.log(tiddler);
+    TiddlyChrome.putTiddler(tiddler);
 };
 
 TiddlyChrome.readTiddler = function() {
-   var typeElem = document.getElementById('type');
-   var type = typeElem.options[typeElem.options.selectedIndex].value;
+    var typeElem = document.getElementById('type');
+    var type = typeElem.options[typeElem.options.selectedIndex].value;
 
-   var tiddler = {};
-   tiddler.title = document.getElementById('title').value;
-   tiddler.text = document.getElementById('text').value;
-   tiddler.tags = document.getElementById('tags').value;
-   tiddler.type = type;
-   tiddler.bag = TiddlyChrome.spaceName + '_' + 'public';
-   return tiddler;
+    var tiddler = {};
+    tiddler.title = document.getElementById('title').value;
+    tiddler.text = document.getElementById('text').value;
+    tiddler.tags = document.getElementById('tags').value;
+    tiddler.type = type;
+    tiddler.bag = TiddlyChrome.spaceName + '_' + 'public';
+    return tiddler;
 };
 
 TiddlyChrome.putTiddler = function(tiddler) {
-   var callBack = function() {
-      if (xhr.readyState == 4) {
-         console.log(xhr.responseText);
-      }
-   };
-   TiddlyChrome.doAjax(TiddlyChrome.baseURL + '/bags/' + tiddler.bag + '/tiddlers/' + tiddler.title,
-                       'PUT', tiddler, callBack);   
+    var callBack = function() {
+        if (xhr.readyState == 4) {
+            console.log(xhr.responseText);
+        }
+    };
+    TiddlyChrome.doAjax(TiddlyChrome.baseURL + '/bags/' + tiddler.bag + '/tiddlers/' + tiddler.title,
+        'PUT', tiddler, callBack);
 };
 
 TiddlyChrome.doAjax = function(url, method, data, callBack) {
-   var xhr = new XMLHttpRequest();
-   xhr.open(method, url, true);
-   xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-   xhr.onreadystatechange = callBack;
-   xhr.send(JSON.stringify(data));   
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = callBack;
+    xhr.send(JSON.stringify(data));
 };
 
 var app = new TiddlyChrome();
 document.getElementById('save').onclick = TiddlyChrome.saveTiddler;
-
